9bc2b5b965a24f85c42bd79d33a79360a5b7c4e2
TristanTango <tristantango09@gmail.com>
2026-03-25 12:49:24 +0800

    Update Lab_8.php

--- Lab_8.php
+++ Lab_8.php
@@ -47,6 +47,15 @@ body {
     margin-top: 10px;
     padding-top: 10px;
 }
+.label{
+    color: gray;
+}
+.thankyou{
+    text-align: center;
+    width: 100%;
+    color: #555;
+}
+
 </style>
 </head>
 
@@ -66,13 +75,64 @@ $quantity=3;
 $price=59999.99;
 $card='123409912316591';
 //DO TASKS HERE
-/* 
-// 1. remove trailing and leading whitespaces then convert the name and item to uppercase
+
+// 1. remove trailing and leading whitespaces then convert the name and item to uppercase//
+$name = strtoupper(trim($name));
+$item = strtoupper(trim($item));
+
 // 2. Compute total
+$total = $price * $quantity;
+
 // 3. Compute VAT (12%)
+$vat = $total * 0.12;
+
 // 4. Compute grand total
+$grand_total =$total + $vat;
+
 // 5. DISPLAY OUTPUT
-// 6. add payment card but display only the first 2 and last 4 digits.
+echo '<div class="line">
+<span class="label">CUSTOMER</span>
+        <span class="value">'.$name.'</span>
+        </div>';
+
+echo '<div class="line">
+<span class="label">ITEM</span>
+        <span class="    value">'.$item.'</span>
+        </div>';
+
+echo '<div class="line">
+        <span class="label">PRICE</span>
+        <span class="value">'.$price.'</span>
+        </div>';
+
+echo '<div class="line">
+        <span class="label">QTY</span>
+        <span class="value">'.$quantity.'</span>
+        </div>';
+
+echo '<div class="line">
+        <span class="label">TOTAL</span>
+        <span class="value">'.$total.'</span>
+        </div>';
+
+echo '<div class="line">
+        <span class="label">VAT (12%)</span>
+        <span class="value">'.number_format($vat, 2).'</span>
+        </div>';
+echo '<div class="line">
+        <span>CARD</span>
+        <span class="value">'.substr($card, 0, 2).'********'.substr($card, -4).'</span>
+        </div>';// 6. add payment card but display only the first 2 and last 4 digits.
+
+echo '<div class="line total">
+        <span class="label">GRAND TOTAL</span>
+        <span class="value">'.number_format($grand_total, 2).'</span>
+        </div>';     
+
+echo '<div class="line">
+        <span class="thankyou">Thank you for your purchase!</span>
+        </div>';
+
 // 7. refer to picture for output.
 
 
@@ -81,9 +141,8 @@ $card='123409912316591';
 
 // REQUIRED LABELS:
 // CUSTOMER, ITEM, PRICE, QTY, TOTAL, VAT (12%), GRAND TOTAL
-utilize this line for the outputs:
-echo '<div class="line"><span class="label">...</span><span class="value">'...'</span></div>';
-*/
+//utilize this line for the outputs:
+//echo '<div class="line"><span class="label">...</span><span class="value">'...'</span></div>';
 // Add class "total" to GRAND TOTAL line
 
 // =====================================
